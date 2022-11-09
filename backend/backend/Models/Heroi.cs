dc7901884fcb7bb49a9e3d0ff27ed680886bf6a6
danielpb93 <danielpb6293@gmail.com>
2022-11-08 23:16:45 -0300

    created models

---
+++ backend/backend/Models/Heroi.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Models
+{
+    public class Heroi
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public string NomeHeroi { get; set; }
+        [Required]
+        public DateTime DataNascimento { get; set; }
+        [Required]
+        public double Altura { get; set; }
+        [Required]
+        public double Peso { get; set; }
+        public List<Superpoder> Superpoderes { get; set; }
+    }
+}
---
+++ backend/backend/Models/Superpoder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Models
+{
+    public class Superpoder
+    {
+        public int Id { get; set; }
+        [Column("Superpoder")]
+        [Required]
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public List<Heroi> Herois { get; set; }
+    }
+}
