47d3771703e1620f1fcb001af9d8d7bbab8e4337
danielpb93 <danielpb6293@gmail.com>
2022-11-10 15:36:56 -0300

    validation for insert and update

--- backend/backend/DTOs/HeroiDTO.cs
+++ backend/backend/DTOs/HeroiDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,17 @@ namespace backend.DTOs
 {
     public class HeroiCreateModel
     {
+        [Required(ErrorMessage ="Nome é obrigatorio!")]
+        [MinLength(1,ErrorMessage ="Nome está vazio!")]
         public string Nome { get; set; }
+        [Required(ErrorMessage = "Nome de heroi é obrigatorio!")]
+        [MinLength(1, ErrorMessage = "Nome de heroi está vazio!")]
         public string NomeHeroi { get; set; }
+        [Required(ErrorMessage = "Data de nascimento é obrigatoria!")]
         public DateTime DataNascimento { get; set; }
+        [Range(0.1, 100, ErrorMessage ="{0} deve ser entre {1} e {2}")]
         public double Altura { get; set; }
+        [Range(1, 1000, ErrorMessage ="{0} deve ser entre {1} e {2}")]
         public double Peso { get; set; }
         public List<int> Superpoderes { get; set; }
     }
