ca014bd1c33d318077b8f577c143087e447d6cbe
danielpb93 <danielpb6293@gmail.com>
2022-11-10 02:44:20 -0300

    added methods to the service

--- frontend/src/app/super-hero/super-hero.service.ts
+++ frontend/src/app/super-hero/super-hero.service.ts
@@ -2,6 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 
+export interface HeroiDto {
+  nome: string;
+  nomeHeroi: string;
+  dataNascimento: Date;
+  altura: number;
+  peso: number;
+  superpoderesIds: number[];
+}
+
 export interface Heroi {
   id: number;
   nome: string;
@@ -9,10 +18,10 @@ export interface Heroi {
   dataNascimento: Date;
   altura: number;
   peso: number;
-  superpoderes: Superpoderes[];
+  superpoderes: Superpoder[];
 }
 
-export interface Superpoderes {
+export interface Superpoder {
   id: number;
   nome: string;
   descricao: string;
@@ -24,7 +33,27 @@ export interface Superpoderes {
 export class SuperHeroService {
   constructor(private http: HttpClient) {}
 
-  get() {
+  getHeroes() {
     return this.http.get<Heroi[]>(`${environment.apiUrl}/heroi`);
+  }
+
+  getHeroById(id: number) {
+    return this.http.get<Heroi>(`${environment.apiUrl}/heroi/${id}`);
+  }
+
+  getSuperpowers() {
+    return this.http.get<Superpoder[]>(`${environment.apiUrl}/superpoder`);
+  }
+
+  createHero(heroi: HeroiDto) {
+    return this.http.post<Heroi>(`${environment.apiUrl}/heroi`, heroi);
+  }
+
+  updateHero(id: number, heroi: HeroiDto) {
+    return this.http.put(`${environment.apiUrl}/heroi/${id}`, heroi);
+  }
+
+  deleteHero(id: number) {
+    return this.http.delete(`${environment.apiUrl}/heroi/${id}`);
   }
 }
